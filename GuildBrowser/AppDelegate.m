8db332e025a2731af111c4c2c22ccdf75958a03b
Filbert Cruz <filbertcruz@phdocnb0218.int.asurion.com>
2015-07-20 16:56:23 +0800

    Remove test

--- GuildBrowser/AppDelegate.m
+++ GuildBrowser/AppDelegate.m
@@ -5,7 +5,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    NSLog(@"This is a test on launch event");
     
     [self customizeAppearance];
     
