b4c3befcf8615346308406965c48863624a71128
Filbert Cruz <filbertcruz@phdocnb0197.int.asurion.com>
2015-07-21 15:51:01 +0800

    Added unit test

---
+++ GuildBrowserLogicTests/WowUtilsTests.m
@@ -0,0 +1,69 @@
+//
+//  WowUtilsTests.m
+//  GuildBrowser
+//
+//  Created by Filbert Cruz on 7/21/15.
+//  Copyright (c) 2015 Charlie Fulton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import "WowUtils.h"
+
+
+@interface WowUtilsTests : XCTestCase
+
+@end
+
+@implementation WowUtilsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+// 1
+-(void)testCharacterClassNameLookup{
+    // 2
+    XCTAssertEqualObjects(@"Warrior",
+                         [WoWUtils classFromCharacterType:1],
+                         @"ClassType should be Warrior");
+    // 3
+    XCTAssertFalse([@"Mage" isEqualToString:[WoWUtils classFromCharacterType:2]]);
+    
+    // 4
+    XCTAssertTrue([@"Paladin" isEqualToString:[WoWUtils classFromCharacterType:2]]);
+    // add the rest as an exercise
+}
+
+- (void)testRaceTypeLookup{
+    XCTAssertEqualObjects(@"Human", [WoWUtils raceFromRaceType:1]);
+    XCTAssertEqualObjects(@"Orc", [WoWUtils raceFromRaceType:2]);
+    XCTAssertFalse([@"Night Elf" isEqualToString:[WoWUtils raceFromRaceType:45]]);
+    // add the rest as an exercise
+}
+
+- (void)testQualityLookup{
+    XCTAssertEqualObjects(@"Grey", [WoWUtils qualityFromQualityType:1]);
+    XCTAssertFalse([@"Purple" isEqualToString:[WoWUtils qualityFromQualityType:10]]);
+    // add the rest as an exercise
+}
+
+@end
